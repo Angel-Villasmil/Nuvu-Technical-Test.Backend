0fb013af132581becb5c22ce048986587bdb3b90
Angel-Villasmil <aavillasmilm@gmail.com>
2021-04-27 10:56:40 -0400

    Update SwaggerConfig.java

--- src/main/java/cc/nuvu/technical/test/backend/swagger/SwaggerConfig.java
+++ src/main/java/cc/nuvu/technical/test/backend/swagger/SwaggerConfig.java
@@ -1,13 +1,15 @@
 package cc.nuvu.technical.test.backend.swagger;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 //@EnableSwagger2
@@ -18,6 +20,17 @@ public class SwaggerConfig {
 	            .select()
 	            .apis(RequestHandlerSelectors.basePackage("cc.nuvu.technical.test.backend.controller"))
 	            .paths(PathSelectors.any())
-	            .build();
+	            .build()
+	            .apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+	    return new ApiInfo(
+	      "PRUEBA TÉCNICA BACKEND", 
+	      "OFERTA LABORAL NUVU - DESARROLLADOR BACK", 
+	      "API Rest - JWT - MySQL - Spring Boot", 
+	      "Version 1.0", 
+	      new Contact("Angel Villasmil", "www.nuvu.cc", "aavillasmil@gmail.com"), 
+	      "www.nuvu.cc", "www.nuvu.cc", Collections.emptyList());
 	}
 }
