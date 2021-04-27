177637aa27c82543c979b8e75a26dc3fefe51fb1
Angel-Villasmil <aavillasmilm@gmail.com>
2021-04-26 20:18:20 -0400

    Encriptado de Contraseña
    Se creo una nueva clase para encriptar las contraseñas en base de datos.

--- src/main/java/cc/nuvu/technical/test/backend/services/UserService.java
+++ src/main/java/cc/nuvu/technical/test/backend/services/UserService.java
@@ -3,13 +3,16 @@ package cc.nuvu.technical.test.backend.services;
 import java.util.ArrayList;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.*;
 
 import cc.nuvu.technical.test.backend.models.UserModel;
 import cc.nuvu.technical.test.backend.repositories.UserRepository;
+import cc.nuvu.technical.test.backend.security.Crypto;
 
 @Service
 public class UserService {
+	BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 	
     @Autowired
     UserRepository userRepository;
@@ -23,6 +26,7 @@ public class UserService {
 	}
     
     public UserModel saveUser(UserModel user){
+    	user.setPassword(Crypto.encrypt(user.getPassword()));
         return userRepository.save(user);
     }
     
@@ -36,6 +40,7 @@ public class UserService {
 	
 	public String updateUser(UserModel userToUpdate){
 		if (userRepository.findById(userToUpdate.getId()).isPresent()){
+			userToUpdate.setPassword(Crypto.encrypt(userToUpdate.getPassword()));
 			userRepository.save(userToUpdate);
 			return "Usuario modificado.";
 			}
@@ -43,7 +48,7 @@ public class UserService {
 	}
 	
 	public boolean validateUser(String user, String pasword) {
-		UserModel userModel = userRepository.getValidUser(user, pasword);
+		UserModel userModel = userRepository.getValidUser(user, Crypto.encrypt(pasword));
 		if (userModel != null) return true;
 		else return false;
 	}
